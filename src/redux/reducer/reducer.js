3e1d1cf05687667799f977ea1737de1ae8d7363d
min2oyo <min2u@outlook.com>
2022-06-11 19:03:50 +0900

    연락처 추가

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,5 +1,15 @@
-let initialState = {};
+let initialState = {
+  ContactList: []
+};
 
-function reducer(state = initialState, action) { }
+function reducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case `ADD_CONTACT`:
+      return { ...state, ContactList: [...state.ContactList, { name: payload.name, phoneNumber: payload.phoneNumber }] };
+    default:
+      return { ...state };
+  }
+}
 
 export default reducer;
