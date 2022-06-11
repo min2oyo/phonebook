c91f253aa52e69b7fbfacb1c548f6c69855426d5
min2oyo <min2u@outlook.com>
2022-06-11 16:59:32 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+// 1. 왼쪽: 연락처 등록 폼, 오른쪽: 연락처 리스트, search 창
+// 2. 리스트에 유저 이름과 전화번호 추가
+// 3. 리스트에 아이템이 몇 개 있는지 표시
+// 4. 사용자가 유저를 이름검색으로 찾을 수 있다.
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
