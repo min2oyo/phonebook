f47a0bde8fcabfd9e6c6b49ab869aa4f8cb87835
min2oyo <min2u@outlook.com>
2022-06-11 17:18:54 +0900

    왼쪽 UI 디자인

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@
 // 3. 리스트에 아이템이 몇 개 있는지 표시
 // 4. 사용자가 유저를 이름검색으로 찾을 수 있다.
 
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ContactForm from './component/ContactForm';
 
 function App() {
   return (
     <>
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </>
   );
 }
